ea76f8b7498275078fffe80955a8a4fa8b4f1cdb
Santiago Ballerini <sballerini@itba.edu.ar>
2023-06-08 16:52:54 -0300

    feat: add tree iinterface

---
+++ src/backend/domain-specific/Tree.java
@@ -0,0 +1,34 @@
+import java.util.function.Function;
+
+public interface Tree<T> {
+
+  void insert(T element);
+
+  void remove(T element);
+
+  <E> Tree<E> reduce(Function<T, E> function);
+
+  boolean isPresent();
+
+  void addTree(Tree<T> tree);
+
+  T max();
+
+  T min();
+
+  T root();
+
+  int height();
+
+  // ===== For drawing =====
+
+  void draw();
+
+  void find();
+
+  void inorder();
+
+  void preorder();
+
+  void postorder();
+}
